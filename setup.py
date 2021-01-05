d922bacf1b9128008511054df19cc363c7592dcc
K-Molloy <kieran.b.molloy@outlook.com>
2021-01-05 20:55:19 +0000

    Adding lib and cython compiler

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import numpy as np
 
 NAME = "whitebeam"
 VERSION = "0.0.1"
-DESCR = "Whitebeam is a programmable decision tree framework."
+DESCR = "Whitebeam is a framework for creating decision tree functions."
 REQUIRES = ["numpy", "cython", "joblib"]
 
 AUTHOR = "Kieran Molloy"
@@ -17,20 +17,19 @@ SRC_DIR = "whitebeam"
 PACKAGES = [SRC_DIR]
 
 ext_1 = Extension(SRC_DIR + ".core._whitebeam",
-                  [SRC_DIR + "/core/_whitebeam.pyx"],
+                  sources=[SRC_DIR + "/core/_whitebeam.pyx"],
                   libraries=[],
                   include_dirs=[np.get_include()])
-EXTENSIONS = [ext_1]
 
 if __name__ == "__main__":
-    setup(install_requires=REQUIRES,
-          packages=PACKAGES,
-          zip_safe=False,
-          name=NAME,
-          version=VERSION,
-          description=DESCR,
-          author=AUTHOR,
-          author_email=EMAIL,
-          cmdclass={"build_ext": build_ext},
-          ext_modules=EXTENSIONS
-          )
+    setup(ext_modules = [ext_1],
+        cmdclass={"build_ext": build_ext},
+        install_requires=REQUIRES,
+        packages=PACKAGES,
+        zip_safe=False,
+        name=NAME,
+        version=VERSION,
+        description=DESCR,
+        author=AUTHOR,
+        author_email=EMAIL
+        )
